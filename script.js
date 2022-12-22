a8f4901b470ac5ea30735bad6542163bd3935ccb
Jovan Kostic <kosticjovan1@gmail.com>
2022-12-22 01:54:18 +0100

    Veliki rad
    Uvodni deo za HTML gotov, uvodni deo za veb programiranje ispisano

---
+++ script.js
@@ -0,0 +1,15 @@
+function skrolNaUvod(){
+    window.scroll({
+        top: 560,
+        left: 0,
+        behavior: 'smooth'
+      });
+}
+
+function skrolPreskociUvod(){
+    window.scroll({
+        top: 1000,
+        left: 0,
+        behavior: 'smooth'
+      });
+}
